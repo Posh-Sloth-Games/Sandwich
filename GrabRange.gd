de7ba031621958f28d1dbe256f980afb99140570
SpellMender <spellmender@gmail.com>
2023-07-09 00:30:00 -0600

    Update GrabRange.gd

--- GrabRange.gd
+++ GrabRange.gd
@@ -20,26 +20,38 @@ func GrabShape():
 			GrabFirstObject()
 	
 func GrabFirstObject():
-	force_shapecast_update()
+	
+	force_shapecast_update() #Ensure that shapecast is updated
+	var i: int = 0
+	var nearest: int = -1
 	for body in collision_result:
-		if body.collider.name.contains("Ragdoll"):
-			heldObject = body.collider
-			heldObject.top_level = false
-			heldObject.linear_velocity = Vector3.ZERO
-			heldObject.angular_velocity = Vector3.ZERO
-			heldObject.sleeping = true
-			print(heldObject.name)
-			heldObject.reparent(self)
-			holding = true
-			pass
-		if body.collider.name.contains("Door"):
-			heldObject = body.collider
-			print(heldObject.name)
-			heldObject.apply_impulse(Vector3.MODEL_REAR * 10)
-			pass
+		print(body.collider.name)
+		if (not body.collider.name.contains("Ragdoll")):
+			i += 1 #prepare to check next index
+			continue #Only evaluate bodies marked ragdoll
+		if nearest == -1:
+			nearest = i
+		if (self.position.distance_to(body.collider.position) < 
+		self.position.distance_to(collision_result[nearest].collider.position)):
+			nearest = i #save nearest body's index
+		i += 1 #prepare to check next index
+	if (nearest != -1):
+		print(collision_result[nearest].collider.name)
+		heldObject = collision_result[nearest].collider #hold the object at nearest body's index
+		# held object logic
+		heldObject.top_level = false
+		heldObject.linear_velocity = Vector3.ZERO
+		heldObject.angular_velocity = Vector3.ZERO
+		heldObject.sleeping = true
+		print(heldObject.name)
+		heldObject.reparent(self)
+		holding = true
+		return
+
 
 func ReleaseHeldObject():
 	heldObject.top_level = true
 	heldObject.sleeping = false
 	heldObject.apply_impulse(self.get_parent().velocity)
+	heldObject = RigidBody3D.new()
 	holding = false
