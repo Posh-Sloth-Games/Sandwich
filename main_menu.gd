cb3a83528787f22639b01b4510d466953600ac77
Matt Mason <matthewmason0220@gmail.com>
2023-07-09 11:39:16 -0600

    fixes

--- main_menu.gd
+++ main_menu.gd
@@ -2,9 +2,6 @@ extends Node2D
 
 @export var mainGameScene : PackedScene
 
-func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
-
 func _on_start_button_up():
 	get_tree().change_scene_to_file(mainGameScene.resource_path)
 
